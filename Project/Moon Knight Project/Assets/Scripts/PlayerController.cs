a735d15864ea0d3eb47c9625360ab69d7cb738aa
dev17sinka <82217333+meof-coding@users.noreply.github.com>
2022-10-13 18:48:02 +0700

    [Underground Map] adding C# code for Running

--- Project/Moon Knight Project/Assets/Scripts/PlayerController.cs
+++ Project/Moon Knight Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Accessible by inspector
+    public float movementSpeed = 5.0f;
+
+    //Not accessible by Inspector
+    private float InputDirection;
+
+    private Rigidbody2D rb;
+
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void CheckInput()
+    {
+        InputDirection = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * InputDirection, rb.velocity.y);
     }
 }
