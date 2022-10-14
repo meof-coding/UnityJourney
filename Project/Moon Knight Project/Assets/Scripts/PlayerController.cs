f24b9788ef9c165ddc747617ee063695c2578334
dev17sinka <82217333+meof-coding@users.noreply.github.com>
2022-10-14 17:12:51 +0700

    Update PlayerController.cs

--- Project/Moon Knight Project/Assets/Scripts/PlayerController.cs
+++ Project/Moon Knight Project/Assets/Scripts/PlayerController.cs
@@ -77,7 +77,7 @@ public class PlayerController : MonoBehaviour
     {
         if (canJump)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            //rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             availableJumpLeft--;
         }
