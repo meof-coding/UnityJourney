54cfeff2c760baef4cec7fb9ac46df137f96842d
meof-coding <tranthe150186@fpt.edu.vn>
2022-10-21 00:20:48 +0700

    [Underground Map] test button for attack

--- Project/Moon Knight Project/Assets/Scripts/UnderGroundMap/PlayerController.cs
+++ Project/Moon Knight Project/Assets/Scripts/UnderGroundMap/PlayerController.cs
@@ -67,6 +67,16 @@ public class PlayerController : MonoBehaviour
         UpdateAnimation();
         CheckIfCanJump();
         onPickUpItem();
+
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed primary button.");
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+
         if (ClimbingAllowed)
         {
             if (isTouchingWalls && !isGrounded && Input.anyKey)
