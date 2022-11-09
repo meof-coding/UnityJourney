02d4881c9369216b3057dce7ff2ce69fd1c27178
meof-coding <tranthe150186@fpt.edu.vn>
2022-11-10 00:43:34 +0700

    [Underground Map] Flying enemy

--- Project/Moon Knight Project/Assets/Scripts/UnderGroundMap/EnemyController.cs
+++ Project/Moon Knight Project/Assets/Scripts/UnderGroundMap/EnemyController.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speed;
+    private GameObject player;
     public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
+    public Transform startingPoint;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player != null)
+        {
+            Chaise();
+            Flip();
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void ReturnStartingPoint()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);
     }
 
     public void TakeDamage(int damage)
@@ -38,5 +55,24 @@ public class EnemyController : MonoBehaviour
         //Disable the enemy
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
+    }
+
+    private void Chaise()
+    {
+        animator.SetTrigger("Chaise");
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
